9f2c5fe0536d7c68165e757219e326bddfce142b
Luke <luke.cole@sky.uk>
2024-08-09 18:32:54 +0100

    [LC] Spring intialize a new springboot service

---
+++ channel-metadata-store/src/main/java/com/channel_metadata_store/ChannelMetadataStoreApplication.java
@@ -0,0 +1,13 @@
+package com.channel_metadata_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelMetadataStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelMetadataStoreApplication.class, args);
+	}
+
+}
---
+++ channel-metadata-store/src/test/java/com/channel_metadata_store/ChannelMetadataStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.channel_metadata_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelMetadataStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

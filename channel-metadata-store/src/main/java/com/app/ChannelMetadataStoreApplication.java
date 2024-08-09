5eb420f62c7bebe5898aba8d8f6c08eebd1ffce9
Luke <luke.cole@sky.uk>
2024-08-09 21:13:42 +0100

    [LC] Clean up imports

--- channel-metadata-store/src/main/java/com/app/ChannelMetadataStoreApplication.java
+++ channel-metadata-store/src/main/java/com/app/ChannelMetadataStoreApplication.java
@@ -1,13 +1,8 @@
 package com.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
-import org.springframework.context.ApplicationContext;
-
-import javax.swing.*;
-import java.nio.channels.Channel;
 
 @SpringBootApplication
 public class ChannelMetadataStoreApplication {

c0f2f8745cfad51bc3afbb007ffbdae12cf495ef
Luke <luke.cole@sky.uk>
2024-08-09 21:04:39 +0100

    [LC] Include unit test for /metrics API

--- channel-metadata-store/src/test/java/com/app/controller/PrivateEndpointsControllerTest.java
+++ channel-metadata-store/src/test/java/com/app/controller/PrivateEndpointsControllerTest.java
@@ -1,15 +1,14 @@
 package com.app.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,5 +31,13 @@ public class PrivateEndpointsControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("UP"));
+    }
+
+    @Test
+    public void whenMetricsEndpointCalled_ShouldReturnMetrics() throws Exception {
+        this.mockMvc.perform(get("/private/metrics"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("jvm.buffer.memory.used")));
     }
 }
